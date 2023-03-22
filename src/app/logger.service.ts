63c67bd3f009722901a3290377ebe367ca13f2a1
ScottYates <scott@yatesframe.com>
2023-03-22 12:03:14 -0500

    formatting change

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Logger {
-  log(msg: any)   { console.log(msg); }
-  error(msg: any) { console.error(msg); }
-  warn(msg: any)  { console.warn(msg); }
+  log(msg: any) {
+    console.log(msg);
+  }
+  error(msg: any) {
+    console.error(msg);
+  }
+  warn(msg: any) {
+    console.warn(msg);
+  }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
